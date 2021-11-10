ba95c7df0c1aec5c464cef45bc04fe44fee6a8ff
Alp4c4 <74106021+Alp4c4@users.noreply.github.com>
2021-11-10 14:32:05 +0700

    a

--- bc-app/src/app/app.module.ts
+++ bc-app/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { BuyComponent } from './buy/buy.component';
+import { HistoryComponent } from './history/history.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    BuyComponent,
+    HistoryComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
